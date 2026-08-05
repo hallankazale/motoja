9ea70fa2d9b8bba52c0ebc89946c2f60f1498518
hallankazale <48835559+hallankazale@users.noreply.github.com>
2026-08-05 17:13:56 -0400

    connect MotoJa frontend to Supabase

---
+++ config.js
@@ -0,0 +1,6 @@
+window.MOTOJA_CONFIG = Object.freeze({
+  supabaseUrl: 'https://pgdpjhjnzcohdixqpbsx.supabase.co',
+  supabasePublishableKey: 'sb_publishable_nJDQjJRs2gT8FHe9TnfeEg_9oquyMpC',
+  city: 'Campo Verde',
+  state: 'MT'
+});
